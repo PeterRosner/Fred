a3ac2b06ea5f6b7bb6a24cf3883a8c1fc9b9f30e
PeterRosner <prosner97@gmail.com>
2021-10-05 12:13:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ohe6za
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ohe6za)
---
+++ index2.js
@@ -0,0 +1,3 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello3 Node.js v${process.versions.node}!`);
